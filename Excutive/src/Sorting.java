f7ae029ec745f8c060d704d2fe6b4f0c228d0755
Mr. Fu <67308879+BIS-Fu@users.noreply.github.com>
2021-08-30 03:25:47 +1000

    select sorting

--- Excutive/src/Sorting.java
+++ Excutive/src/Sorting.java
@@ -1,16 +1,49 @@
 import java.util.Arrays;
 
-public class Sorting {
+public  class Sorting {
+    /**
+     * 输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+     * @param input
+     * @return
+     */
+    public static int[] poping(int[] input){
+        //比较数字首位，找出最小的所有
+        //如果首位数字都相同，第二位做比较，没有第二位数字视为最大，按照最小排序
+        //如果第二位相同，遵循第二部
+        //tips:使用compareTO而不是boolean原因是comparTo不会栈溢出，当数字过大时。
+        for(int i=0;i< input.length-1;i++){
+            for(int j=0;j< input.length-1-i;j++){
+                if ((""+input[j]+""+input[j+1]).compareTo(""+input[j+1]+""+input[j])>0){
+                    int temp= input[j];
+                    input[j]= input[j+1];
+                    input[j+1]=temp;
+                }
+            }
+        }
 
-    public static int [] poping(int[] input){
-        return null;
+        return input;
     }
 
-    public static void main(String[] args) {
-        int [] adverse = {1,2,3,4,5};
-        int [] adverseOut = {1,2,3,4,5};
-        System.out.println(Arrays.equals(adverse,adverseOut));
+    /**
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * @param nums
+     * @return
+     */
+    public int [] moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length-1-i;j++){
+                if(nums[j]+nums[j+1]==nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+        return nums;
     }
+
+
+
 
 
 }
--- Test/src/SortingTest.java
+++ Test/src/SortingTest.java
@@ -11,15 +11,12 @@ public class SortingTest {
     @Test
     public void testNormal(){
         Sorting sorting = new Sorting();
-        int [] adverse = {5,4,3,2,1};//翻转顺序
-        int [] adverseOut = {1,2,3,4,5};
-        int [] random = {4,1,3,2,5};//随机排列
-        int [] randomOut = {1,2,3,4,5};
-        int [] repeat = {5,4,3,3,2,1};//存在重复值
-        int [] repeatOut = {1,2,3,3,4,5};
+        int [] adverse = {10,2};//翻转顺序
+        int [] adverseOut = {10,2};
+        int [] random = {3,30,34,5,9};//随机排列
+        int [] randomOut = {30,3,34,5,9};
         assertTrue(Arrays.equals(adverse,adverseOut),"For array \"" + Arrays.toString(adverseOut) + "\", but got '" + Arrays.toString(Sorting.poping(adverse)) + "'");
-        assertTrue(Arrays.equals(random,randomOut),"For array \"" + Arrays.toString(randomOut) + "\",  but got '" + Arrays.toString(Sorting.poping(repeatOut)) + "'");
-        assertTrue(Arrays.equals(repeat,repeatOut),"For array \"" + Arrays.toString(repeatOut) + "\",  but got '" + Arrays.toString(Sorting.poping(repeatOut)) + "'");
+        assertTrue(Arrays.equals(random,randomOut),"For array \"" + Arrays.toString(randomOut) + "\",  but got '" + Arrays.toString(Sorting.poping(randomOut)) + "'");
     }
 
 
